64a9eca03495a531f7da7afcb90dd80d89f8cf47
Teodor Kolev <teodorkolev007@gmail.com>
2024-02-04 15:43:42 +0200

    added pattern oracle

--- Lotto/main.py
+++ Lotto/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+def count_up_to(limit):
+    count = 1
+    while count <= limit:
+        yield count
+        count += 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Using the generator
+counter = count_up_to(5)
+for num in counter:
+    print(num)
