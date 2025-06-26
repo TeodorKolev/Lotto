e87b19348900ba6ebf68e42373c6d83a669f86b8
Teodor Kolev <teodorkolev007@gmail.com>
2025-06-26 16:23:13 +0300

    EM env created

--- main.py
+++
@@ -1,9 +0,0 @@
-from environments import ENVIRONMENTS
-
-# Set your environment here
-ENV = "fdj"  # or "test", etc.
-
-config = ENVIRONMENTS[ENV]
-url_to_zip = config["url_to_zip"]
-file_name = config["file_name"]
-row_range = config["row_range"]
